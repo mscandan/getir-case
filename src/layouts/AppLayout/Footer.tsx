ce4900d804584c072f5140c504f531039b3d4151
M. Selcuk Candan <mehmetselcukcandan@icloud.com>
2022-09-27 18:33:12 +0300

    feat: add footer component

---
+++ src/layouts/AppLayout/Footer.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledFooter = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 40px;
+`;
+
+const StyledLink = styled.a`
+  font-size: 13px;
+  line-height: 23px;
+  color: ${({ theme }) => theme.COMPONENT_BG.BASE};
+  font-family: 'Open Sans', sans-serif;
+  font-weight: normal;
+  text-decoration: none;
+`;
+
+const StyledDot = styled.div`
+  margin: 0 16px;
+  color: ${({ theme }) => theme.COMPONENT_BG.BASE};
+`;
+
+export const Footer = () => {
+  return (
+    <StyledFooter>
+      <StyledLink href="#">©2019 Market</StyledLink>
+      <StyledDot>•</StyledDot>
+      <StyledLink href="#">Privacy Policy</StyledLink>
+    </StyledFooter>
+  );
+};
